9193304bf3cbea1e1feab5018e6091b1efd1437f
Nataliia K <kozlova.natalie.1987@gmail.com>
2020-10-19 21:53:35 +0300

    Modified

--- UnitTestProject1_WebDriver_Study/UnitTest1.cs
+++ UnitTestProject1_WebDriver_Study/UnitTest1.cs
@@ -1,14 +1,51 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
-namespace UnitTestProject1_WebDriver_Study
+
+namespace WebDriver123
 {
     [TestClass]
-    public class UnitTest1
+    public class TestSuit
     {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
+            string currentWindowHandle = driver.CurrentWindowHandle;
+            var WindowHandles = driver.WindowHandles;
+
+            driver.FindElement(By.CssSelector("someSelector")); // = драйвер, найди элемент по цсс селектору
+                                                                // если элемент не найден, вернет эксепшен, поэтому нужно обернуть в try catch
+            driver.FindElements(By.CssSelector("someSelector")); // коллекция  элементов; если ни один элемент не будет найден,
+                                                                 // вернется пустая коллекция          
+            driver.Navigate().GoToUrl("www.test.com");
+
+            //driver.Manage().Cookies.DeleteCookie();
+
+            driver.Manage().Window.FullScreen();
+            //driver.Manage().Window.Size();
+            driver.SwitchTo().Alert();
+            //driver.SwitchTo().Frame(); // преключиться в айфрейм
+            driver.SwitchTo().DefaultContent(); // переключиться из айфрейма назад в предыдущее окно
+            driver.SwitchTo().Window(currentWindowHandle);
+
+            Console.WriteLine(currentWindowHandle);
         }
     }
 }
